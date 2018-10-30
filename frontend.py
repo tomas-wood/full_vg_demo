77bae5a5862388b028129d3c44b311a10aa0676d
Thomas Wood <thomas.wood@huawei.com>
2018-10-30 15:41:03 -0700

    used  inside  instead of root repo directory'

--- frontend.py
+++ frontend.py
@@ -1,5 +1,7 @@
 from app import app
-
+from gevent.pywsgi import WSGIServer
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    #app.run(debug=True, host='0.0.0.0',threaded=True)
+    http_server = WSGIServer(('0.0.0.0',5000), app)
+    http_server.serve_forever()
 
